b8e9026696ed39b85f717fdb2ec5724e1186b7e7
cavalliniph <paulo.cavallini08@gmail.com>
2026-06-09 14:36:10 -0300

    test: quebrando o teste de propósito para validar o CI

--- tests/unit/test_senha.py
+++ tests/unit/test_senha.py
@@ -2,7 +2,7 @@ import pytest
 from funcao import validar_senha
 
 def test_validar_senha_minusculo():
-    assert validar_senha("@ph301208") == False
+    assert validar_senha("@ph301208") == True
 
 def test_validar_senha_correta():
     assert validar_senha("@Ph301208") == True
